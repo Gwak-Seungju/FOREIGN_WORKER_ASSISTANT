e48fa53256a69b43e59f4b951db30c3b2af650db
곽승주 <tmwn1015@naver.com>
2025-05-10 10:07:31 +0900

    remove: next framework 삭제

--- components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  const currentYear = new Date().getFullYear();
-  return (
-    <div className="text-center">
-      COPYRIGHT &copy; {currentYear} BY Service Name
-    </div>
-  )
-}
