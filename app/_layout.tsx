0dc417c5107dff6ea0b641d675a9180e37b1edca
곽승주 <tmwn1015@naver.com>
2025-05-15 15:09:23 +0900

    feat: safearea 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { Entypo } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import { Stack } from "expo-router";
 import { useEffect, useState } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -14,12 +15,16 @@ export default function RootLayout() {
 
   if (!fontLoaded) return null;
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-      <Stack.Screen name="chatbot" options={{ headerShown: false}} />
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-      <Stack.Screen name="initial-setup" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+          <Stack.Screen name="chatbot" options={{ headerShown: false}} />
+          <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+          <Stack.Screen name="initial-setup" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
